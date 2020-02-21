1e1e506278ef743fa524ccfaa3d415c5aa2ece7c
Michal Szczepaniak <michal.szczepaniak@inspirelabs.pl>
2020-02-21 18:41:14 +0100

    position sticky first column

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,17 +1,27 @@
-const promise1 = new Promise(function(resolve, reject) {
-    setTimeout(function() {
-        resolve('foo');
-    }, 7000);
-});
 
 
-promise1.then(function(value) {
-    console.log(value);
-    // expected output: "foo"
-});
 
-console.log(promise1);
-// expected output: [object Promise]
+
+
+
+
+
+
+
+// const promise1 = new Promise(function(resolve, reject) {
+//     setTimeout(function() {
+//         resolve('foo');
+//     }, 7000);
+// });
+//
+//
+// promise1.then(function(value) {
+//     console.log(value);
+//     // expected output: "foo"
+// });
+//
+// console.log(promise1);
+// // expected output: [object Promise]
 
 
 
